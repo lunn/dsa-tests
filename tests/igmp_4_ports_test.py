49dc296c9192258adfffe38dcdf3a33a323cf3da
Andrew Lunn <andrew@lunn.ch>
2017-07-09 17:39:15 -0500

    igmp_4_ports: Add test for igmp snooping
    The software bridge should perform IGMP snooping and program the
    switch which ports to send multicast frames out of.

---
+++ tests/igmp_4_ports_test.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+"""Test the operation of IGMP on a bridge on the SUT"""
+
+import time
+import unittest2
+import xmlrunner
+
+import host
+import params
+import sut
+import traffic
+
+
+SUT = None
+HOST = host
+TRAFFIC = None
+CONFIG = None
+VLAN_FILTERING = False
+
+ethtool_rx_0_tx_0 = {'in_multicasts': (0, 4),
+                     'out_multicasts': (0, 4)}
+
+ethtool_rx_5_tx_0 = {'in_multicasts': (5, 9),
+                     'out_multicasts': (0, 4)}
+
+ethtool_rx_0_tx_5 = {'in_multicasts': (0, 4),
+                     'out_multicasts': (5, 9)}
+
+class igmp_4_port_test(unittest2.TestCase):
+    '''Class containing the test cases'''
+
+    def setUp(self):
+        """Setup ready to perform the test"""
+        self.sut = SUT
+        self.host = HOST
+        self.traffic = TRAFFIC
+        self.config = CONFIG
+        self.maxDiff = None
+        self.vlan_filtering = VLAN_FILTERING
+
+    def test_01_setup_sut(self):
+        """Create the bridge"""
+        # Ensure all the interfaces are up
+        self.sut.up(self.config.SUT_MASTER)
+        self.sut.up(self.config.SUT_LAN0)
+        self.sut.up(self.config.SUT_LAN1)
+        self.sut.up(self.config.SUT_LAN2)
+        self.sut.up(self.config.SUT_LAN3)
+
+        self.sut.addBridge('br1')
+        self.sut.up('br1')
+        self.sut.addAddress('br1', '192.168.58.42/24')
+        self.sut.addBridgeInterface('br1', self.config.SUT_LAN0)
+        self.sut.addBridgeInterface('br1', self.config.SUT_LAN1)
+        self.sut.addBridgeInterface('br1', self.config.SUT_LAN2)
+
+        # Wait the forwarding delay of the bridge
+        time.sleep(10)
+
+    def test_02_setup_host(self):
+        """Setup on the host, perform learning"""
+        self.host.addInterface(self.config.HOST_LAN0)
+        self.host.addInterface(self.config.HOST_LAN1)
+        self.host.addInterface(self.config.HOST_LAN2)
+        self.host.addInterface(self.config.HOST_LAN3)
+        self.host.cleanSystem()
+
+        self.host.up(self.config.HOST_LAN0)
+        self.host.up(self.config.HOST_LAN1)
+        self.host.up(self.config.HOST_LAN2)
+        self.host.up(self.config.HOST_LAN3)
+
+        self.host.addAddress(self.config.HOST_LAN0, '192.168.58.1/24')
+        self.host.addAddress(self.config.HOST_LAN1, '192.168.58.2/24')
+        self.host.addAddress(self.config.HOST_LAN2, '192.168.58.3/24')
+
+        if self.vlan_filtering:
+            self.sut.bridgeEnableVlanFiltering('br1')
+
+        # Send learning packets, so the bridge knows which MAC address
+        # is where
+        self.traffic.addInterface(self.config.HOST_LAN0)
+        self.traffic.addInterface(self.config.HOST_LAN1)
+        self.traffic.addInterface(self.config.HOST_LAN2)
+        self.traffic.addInterface(self.config.HOST_LAN3)
+        self.traffic.learning()
+
+    def test_03_join(self):
+        """Join a group on LAN1 and LAN2. LAN0 is does not join, so snooping
+           should mean it does not receive multicast frames for this group"""
+
+        self.host.join(self.config.HOST_LAN1, '192.168.58.2', '224.42.42.42')
+        self.host.join(self.config.HOST_LAN2, '192.168.58.3', '224.42.42.42')
+
+    def test_04_sleep(self):
+        """Sleep for a while to allow IGMP to be exchanged"""
+        time.sleep(10)
+
+    def test_05_multicast_lan1(self):
+        """Send some multicast packets out LAN1. We expect to receive them on
+           LAN2, but not LAN0. LAN3 is not part of the bridge, it
+           should not receive them either."""
+
+        ethtool_stats_lan0 = self.sut.getEthtoolStats(self.config.SUT_LAN0)
+        ethtool_stats_lan1 = self.sut.getEthtoolStats(self.config.SUT_LAN1)
+        ethtool_stats_lan2 = self.sut.getEthtoolStats(self.config.SUT_LAN2)
+        ethtool_stats_lan3 = self.sut.getEthtoolStats(self.config.SUT_LAN3)
+
+        self.traffic.addUDPMulticastStream(self.config.HOST_LAN1,
+                                           '224.42.42.42', 5, 5)
+        self.traffic.run()
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN0,
+                                        ethtool_stats_lan0,
+                                        ethtool_rx_0_tx_0, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN1,
+                                        ethtool_stats_lan1,
+                                        ethtool_rx_5_tx_0, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN2,
+                                        ethtool_stats_lan2,
+                                        ethtool_rx_0_tx_5, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN3,
+                                        ethtool_stats_lan3,
+                                        ethtool_rx_0_tx_0, self)
+
+
+    def test_06_multicast_lan2(self):
+        """Send some multicast packets out LAN2. We expect to receive them on
+           LAN1, but not LAN0.  LAN3 is not part of the bridge, it
+           should not receive them either."""
+
+        ethtool_stats_lan0 = self.sut.getEthtoolStats(self.config.SUT_LAN0)
+        ethtool_stats_lan1 = self.sut.getEthtoolStats(self.config.SUT_LAN1)
+        ethtool_stats_lan2 = self.sut.getEthtoolStats(self.config.SUT_LAN2)
+        ethtool_stats_lan3 = self.sut.getEthtoolStats(self.config.SUT_LAN3)
+
+        self.traffic.addUDPMulticastStream(self.config.HOST_LAN2,
+                                           '224.42.42.42', 5, 5)
+        self.traffic.run()
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN0,
+                                        ethtool_stats_lan0,
+                                        ethtool_rx_0_tx_0, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN1,
+                                        ethtool_stats_lan1,
+                                        ethtool_rx_0_tx_5, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN2,
+                                        ethtool_stats_lan2,
+                                        ethtool_rx_5_tx_0, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN3,
+                                        ethtool_stats_lan3,
+                                        ethtool_rx_0_tx_0, self)
+
+    def test_07_multicast_lan0(self):
+        """Send some multicast packets out LAN0. We expect to receive them on
+           LAN1 and LAN2. LAN3 is not part of the bridge, it should
+           not receive them either."""
+
+        ethtool_stats_lan0 = self.sut.getEthtoolStats(self.config.SUT_LAN0)
+        ethtool_stats_lan1 = self.sut.getEthtoolStats(self.config.SUT_LAN1)
+        ethtool_stats_lan2 = self.sut.getEthtoolStats(self.config.SUT_LAN2)
+        ethtool_stats_lan3 = self.sut.getEthtoolStats(self.config.SUT_LAN3)
+
+        self.traffic.addUDPMulticastStream(self.config.HOST_LAN0,
+                                           '224.42.42.42', 5, 5)
+        self.traffic.run()
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN0,
+                                        ethtool_stats_lan0,
+                                        ethtool_rx_5_tx_0, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN1,
+                                        ethtool_stats_lan1,
+                                        ethtool_rx_0_tx_5, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN2,
+                                        ethtool_stats_lan2,
+                                        ethtool_rx_0_tx_5, self)
+
+        self.sut.checkEthtoolStatsRange(self.config.SUT_LAN3,
+                                        ethtool_stats_lan3,
+                                        ethtool_rx_0_tx_0, self)
+
+    def test_99_delete_bridge(self):
+        """Destroy the bridge"""
+        self.sut.deleteBridgeInterface('br1', self.config.SUT_LAN0)
+        self.sut.deleteBridgeInterface('br1', self.config.SUT_LAN1)
+        self.sut.deleteBridgeInterface('br1', self.config.SUT_LAN2)
+        self.sut.deleteBridge('br1')
+
+        # Ensure all the interfaces are down
+        self.sut.down(self.config.SUT_LAN0)
+        self.sut.down(self.config.SUT_LAN1)
+        self.sut.down(self.config.SUT_LAN2)
+        self.sut.down(self.config.SUT_LAN3)
+
+
+if __name__ == '__main__':
+    args = params.params()
+    CONFIG = params.readConfig(args.config)
+    SUT = sut.SUT(hostname=CONFIG.hostname, key=CONFIG.key,
+                  mgmt=CONFIG.SUT_MGMT)
+    SUT.cleanSystem()
+    HOST = host.HOST()
+    TRAFFIC = traffic.Traffic()
+
+    if args.xml:
+        testRunner = xmlrunner.XMLTestRunner(output='test-reports',
+                                             verbosity=args.verbose)
+    else:
+        testRunner = unittest2.TextTestRunner(failfast=args.failfast,
+                                              verbosity=args.verbose)
+    if args.vlanfiltering:
+        VLAN_FILTERING = True
+
+    unittest2.main(buffer=False, testRunner=testRunner, exit=False)
+    HOST.cleanSystem()
